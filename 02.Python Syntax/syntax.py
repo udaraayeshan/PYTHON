cd016e875db8fc9dcd59eaae5d617d3e69c334c0
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-13 22:27:38 +0530

    Syntax

---
+++ 02.Python Syntax/syntax.py
@@ -0,0 +1,26 @@
+
+  #if 5 > 2:
+  #print("Five is greater than two!")  /IndentationError: unexpected indent
+  
+  
+
+if 5 > 2:
+        print("Five is greater than two!") 
+     
+if 5 > 2:   
+             print("Five is greater than two!")  # No error
+        
+        
+if 5 > 2:
+     print("Five is greater than two!")
+     #   print("Five is greater than two!")     / IndentationError: unexpected indent
+     
+     
+     
+     #Python Variables 
+     
+x = 5
+y = "Hello, World!"
+
+print(x)
+print(y)
