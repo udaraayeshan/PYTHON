3273e7141f685e36e0f195f4d9440c5fa35206f7
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-13 22:34:21 +0530

    Comments

---
+++ 03.Python Comments/Comments.py
@@ -0,0 +1,34 @@
+"""
+Python Comments
+Comments can be used to explain Python code.
+
+Comments can be used to make the code more readable.
+
+Comments can be used to prevent execution when testing code.
+
+Creating a Comment
+Comments starts with a #, and Python will ignore them:
+
+Example
+
+"""
+
+#single Line Comments
+
+
+#This is a comment
+print("Hello, World!")
+
+print("Hello, World!") #This is a comment
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+
+#Multi Line Comments
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
