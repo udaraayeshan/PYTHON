f4cd644755bd58d3dd56586a74ea9007d6110413
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-18 21:44:08 +0530

    Slicing Strings

---
+++ 08.Python Strings/2.SlicingStrings.py
@@ -0,0 +1,44 @@
+# Slicing
+# You can return a range of characters by using the slice syntax.
+
+# Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+# Example
+# Get the characters from position 2 to position 5 (not included):
+
+b = "Hello, World!"
+print(b[1:8])
+#Note: The first character has index 0.
+
+
+# Slice From the Start
+# By leaving out the start index, the range will start at the first character:
+
+# Example
+# Get the characters from the start to position 5 (not included):
+
+b = "Hello, World!"
+print(b[:5])
+
+
+# Slice To the End
+# By leaving out the end index, the range will go to the end:
+
+# Example
+# Get the characters from position 2, and all the way to the end:
+
+b = "Hello, World!"
+print(b[2:])
+
+# Negative Indexing
+# Use negative indexes to start the slice from the end of the string:
+# Example
+# Get the characters:
+
+# From: "o" in "World!" (position -5)
+
+# To, but not included: "d" in "World!" (position -2):
+
+b = "Hello, World!"
+print(b[-6:-3])    #awasana eka nathuwa out put eka deyi
+
