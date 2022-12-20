6f2777677b585e611edac707b39940b711a8279c
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-21 00:56:51 +0530

    Remove list

---
+++ 11.Python Lists/RemoveListItems.py
@@ -0,0 +1,37 @@
+
+
+# Remove Specified Item
+# The remove() method removes the specified item.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+#The pop() method removes the specified index.
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+#Remove the last item:
+
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+#The del keyword also removes the specified index:
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+#The del keyword can also delete the list completely.
+
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+
+# Clear the List
+# The clear() method empties the list.
+
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
