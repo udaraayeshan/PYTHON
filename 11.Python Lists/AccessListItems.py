b5302dc23e85594bc9d66cffc3535aa9bf4131c4
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-21 00:18:28 +0530

    Access List

---
+++ 11.Python Lists/AccessListItems.py
@@ -0,0 +1,20 @@
+
+# Print the second item of the list:
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#Negative Indexing
+thislist = ["apple", "banana", "cherry"]
+print(thislist[-1])
+
+
+
+#Range of Indexes
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[:4])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist[2:])
