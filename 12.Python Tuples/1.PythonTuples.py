3410bcd1453a0226fe15fa993bb5dd64715efcc4
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-24 12:57:56 +0530

    Tuples

---
+++ 12.Python Tuples/1.PythonTuples.py
@@ -0,0 +1,27 @@
+
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+
+# A tuple with strings, integers and boolean values:
+tuple1 = ("abc", 34, True, 40, "male")
+
+print(tuple1)
+
+
+# type()
+
+mytuple = ("apple", "banana", "cherry")
+print(type(mytuple))
+
+
+
+# The tuple() Constructor
+# It is also possible to use the tuple() constructor to make a tuple.
+
+thistuple = tuple(("apple", "banana", "cherry")) # note the double round-brackets
+print(thistuple)
