5e3e8ec1c1fe28d879fb2a17f616a42686b24946
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-24 12:14:17 +0530

    ListComprehension

--- 11.Python Lists/LoopLists.py
+++ 11.Python Lists/LoopLists.py
@@ -24,3 +24,42 @@ while i < len(thislist):
 thislist = ["apple", "banana", "cherry"]
 [print(x) for x in thislist]
 
+
+
+#me akaraatarh liyanna puluwan
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if "a" in x]
+
+print(newlist)
+
+#The Syntax
+#newlist = [expression for item in iterable if condition == True]
+
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x for x in fruits if x != "apple"] #condision 
+
+print(newlist)
+
+# You can use the range() function to create an iterable:
+
+newlist = [x for x in range(10)]
+
+print(newlist)
+
+
+#newlist = [x for x in range(10) if x < 5]
+
+
+# Expression
+fruits = ["apple", "banana", "cherry", "kiwi", "mango"]
+
+newlist = [x.upper() for x in fruits]
+
+print(newlist)
+
+
+
+
+
