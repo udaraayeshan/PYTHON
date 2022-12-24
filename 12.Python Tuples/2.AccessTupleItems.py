03109664034f97f0420f2867f3ab4781e8b3674f
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-24 13:04:41 +0530

    AccessTuples

---
+++ 12.Python Tuples/2.AccessTupleItems.py
@@ -0,0 +1,26 @@
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+
+# Range of Indexes
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[:4])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+
+#Check if Item Exists
+thistuple = ("apple", "banana", "cherry")
+if "apple" in thistuple:
+  print("Yes, 'apple' is in the fruits tuple")
+
