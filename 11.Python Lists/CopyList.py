22d1a979bc10005b8ccac7c33043a1cf4d6928c0
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-24 12:40:09 +0530

    LOOPLIST

---
+++ 11.Python Lists/CopyList.py
@@ -0,0 +1,12 @@
+# Copy Lists
+
+thislist = ["apple", "banana", "cherry"]
+mylist = thislist.copy()
+print(mylist)
+
+#Another way to make a copy is to use the built-in method list().thislist = ["apple", "banana", "cherry"]
+# Make a copy of a list with the list() method:
+
+thislist = ["apple", "banana", "cherry"]
+mylist = list(thislist)
+print(mylist)
