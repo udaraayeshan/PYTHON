53fa14f34562a372a2e69a83d5dcf2f9d7730671
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-24 13:40:59 +0530

    LoopTUples

---
+++ 12.Python Tuples/5.LoopTuples.py
@@ -0,0 +1,19 @@
+
+#Loop Through a Tuple
+thistuple = ("apple", "banana", "cherry")
+for x in thistuple:
+  print(x)
+  
+#Loop Through the Index Numbers
+thistuple = ("apple", "banana", "cherry")
+for i in range(len(thistuple)):
+  print(thistuple[i])
+  
+  
+#Using a While Loop
+thistuple = ("apple", "banana", "cherry")
+i = 0
+while i < len(thistuple):
+  print(thistuple[i])
+  i = i + 1
+  
