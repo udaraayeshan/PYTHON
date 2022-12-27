ce02073ec7a30349304e81d2b0699aed5420aa78
AyeshanAnuradha <ayeshan98udara@gmail.com>
2022-12-27 22:51:23 +0530

    Iterators

---
+++ 23.Python Iterators/lterators.py
@@ -0,0 +1,109 @@
+'''Python Iterators
+An iterator is an object that contains a countable number of values.
+
+An iterator is an object that can be iterated upon, meaning that you can traverse through all the values.
+
+Technically, in Python, an iterator is an object which implements the iterator protocol, which consist of the methods __iter__() and __next__().'''
+
+
+# Iterator vs Iterable
+# Lists, tuples, dictionaries, and sets are all iterable objects. They are iterable containers which you can get an iterator from.
+
+# All these objects have a iter() method which is used to get an iterator:
+
+# Example
+# Return an iterator from a tuple, and print each value:
+
+mytuple = ("apple", "banana", "cherry")
+myit = iter(mytuple)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+
+
+# Even strings are iterable objects, and can return an iterator:
+
+# Example
+# Strings are also iterable objects, containing a sequence of characters:
+
+mystr = "banana"
+myit = iter(mystr)
+
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+print(next(myit))
+
+#Looping Through an Iterator
+
+# Looping Through an Iterator
+# We can also use a for loop to iterate through an iterable object:
+
+# Example
+# Iterate the values of a tuple:
+
+mytuple = ("apple", "banana", "cherry")
+
+for x in mytuple:
+  print(x)
+  
+  
+# Iterate the characters of a string:
+
+mystr = "banana"
+
+for x in mystr:
+  print(x)
+  
+#The for loop actually creates an iterator object and executes the next() method for each loop.
+  
+
+
+# Create an Iterator
+class MyNumbers:
+      def __iter__(self):
+       self.a = 1
+       return self
+
+def __next__(self):
+    x = self.a
+    self.a += 1
+    return x
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+print(next(myiter))
+
+
+# StopIteration
+
+# Example
+# Stop after 20 iterations:
+
+class MyNumbers:
+  def __iter__(self):
+    self.a = 1
+    return self
+
+  def __next__(self):
+    if self.a <= 20:
+      x = self.a
+      self.a += 1
+      return x
+    else:
+      raise StopIteration
+
+myclass = MyNumbers()
+myiter = iter(myclass)
+
+for x in myiter:
+  print(x)
