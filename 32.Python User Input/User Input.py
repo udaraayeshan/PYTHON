eb5d8441412eca0f84e156a0e36b87e4dbe1fb6b
AyeshanAnuradha <ayeshan98udara@gmail.com>
2023-01-02 23:34:41 +0530

    User Input

---
+++ 32.Python User Input/User Input.py
@@ -0,0 +1,16 @@
+
+
+# Python 3.6 uses the input() method.
+
+# Python 2.7 uses the raw_input() method.
+
+
+
+# Python 3.6
+username = input("Enter username:")
+print("Username is: " + username)
+
+
+# Python 2.7
+# username = raw_input("Enter username:")
+# print("Username is: " + username)
